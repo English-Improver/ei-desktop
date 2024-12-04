5e781d5e2736df5e2d09f3dffca2aacdb7ed7ca7
songyitian <yitiansong4@gmail.com>
2024-12-04 11:09:39 +0800

    xx

--- vite.preload.config.mjs
+++ vite.preload.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import path from "path";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  resolve: {
+    // 避免 Vite 试图打包 Electron
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["electron"],
+    },
+    ssr: true,
+  },
+});
